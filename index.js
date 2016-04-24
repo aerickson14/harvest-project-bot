23bd02e95e16489bd4a8e49eb44f881ff5067030
Andrew Erickson <aerickson14@gmail.com>
2016-04-23 21:34:57 -0500

    harvest bot implementation

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var Botkit = require('botkit')
+var Harvest = require('harvest');
+var config = require('nconf');
+
+config.argv().env().file({file: 'config/default.json'});
+var harvest = new Harvest({
+    subdomain: config.get('harvest:subdomain'),
+    email: config.get('harvest:email'),
+    password: config.get('harvest:password')
+});
 
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
@@ -18,7 +27,44 @@ bot.startRTM(function (err, bot, payload) {
   }
 })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-    console.log("Heard a mention");
-    bot.replyPrivate(message, 'Hi')
-})
+controller.hears(['projects'], ['direct_message', 'direct_mention'], function(bot, message) {
+    
+    harvest.Projects.list({}, function(err, projects) {
+        var requests = 0;
+        var responseMessage = "";
+        if (err) {
+            console.log("Error");
+            console.log(err);
+            responseMessage = "There was an error retrieving projects from Harvest";
+        } else {
+            var projectNames = {};
+            var numProjects = projects.length;
+            for (var i=0; i < numProjects; i++) {
+                var project = projects[i]["project"];
+                if (project) {
+                    requests++;
+                    projectNames[project.client_id] = project.name;
+                    harvest.Clients.get({id: project.client_id}, function(err, clients) {
+                        requests--;
+                        if (err) {
+                            console.log("Error");
+                            console.log(err);
+                        } else {
+                            var client = clients["client"];
+                            responseMessage += client.name + ": " + projectNames[client.id] + "\n";
+                        }
+                    });
+                }
+            }
+            
+            var responseInterval = setInterval(function() {
+                if (requests == 0) {
+                    clearInterval(responseInterval);
+                    bot.startPrivateConversation({user: message.user}, function(err, dm) {
+                        dm.say(responseMessage);
+                    });
+                }
+            }, 100);
+        }
+    });
+});
